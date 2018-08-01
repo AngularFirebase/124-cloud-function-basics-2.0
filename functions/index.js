16e2c7a7e95db1bd13403cbf4187bc67e7b1b391
codediodeio <hello@codediode.io>
2018-08-01 14:09:29 -0700

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,64 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+var Filter = require('bad-words'),
+    filter = new Filter();
+
+
+
+
+exports.badWords = functions.firestore
+
+    .document('tweets/{tweetId}')
+    .onCreate(async(snapshot, context) => {
+
+        const data = snapshot.data();
+
+        const clean = filter.clean(data.content)
+
+        if (clean !== data.content) {
+            const user = admin.firestore().doc(`users/${data.userId}`)
+            await user.update({ status: 'BANNED' })
+        }
+
+        return snapshot.ref.update({ content: clean })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.myFirstFunction = functions.https.onRequest((req, res) => {
+
+    const name = req.query.name;
+
+    if (!name) {
+        res.status(400).send('missing name!')
+    }
+
+
+    res.send(name.split('').reverse().join(''))
+
+})
